af564f7dc030a930e3538c6f887221c7d93ad760
benwilcock <benwilcock@gmail.com>
2017-07-25 09:00:43 -0600

    Adding support for config server

--- create-cf-services.sh
+++ create-cf-services.sh
@@ -9,3 +9,5 @@ cf create-service p-identity p-identity tracker-sso
 
 cf create-service p-circuit-breaker-dashboard standard tracker-circuit-breaker-dashboard
 cf create-service p-rabbitmq standard tracker-amqp
+
+cf create-service p-config-server standard tracker-config-server -c '{"git": {"uri": "https://github.com/benwilcock/tracker-config.git", "label": "master"}}'
