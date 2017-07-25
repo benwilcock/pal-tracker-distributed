876f174f8f4dff8d8cf76a3a7d07863c323c5bc2
benwilcock <benwilcock@gmail.com>
2017-07-25 06:57:35 -0600

    Added Circuit Breaker code

--- create-cf-services.sh
+++ create-cf-services.sh
@@ -6,3 +6,6 @@ cf create-service p-mysql 100mb tracker-timesheets-database
 
 cf create-service p-service-registry standard tracker-service-registry
 cf create-service p-identity p-identity tracker-sso
+
+cf create-service p-circuit-breaker-dashboard standard tracker-circuit-breaker-dashboard
+cf create-service p-rabbitmq standard tracker-amqp
